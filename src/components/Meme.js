bb3ffcbfe75b85deb72ab1aa74430068e7fe51c5
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-22 14:38:30 +0500

    Add images to the memes generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,22 +2,32 @@ import React from 'react'
 import memesData from '../memesData'
 
 export default function Meme() {
-  let url;
+  const [memeImage, setMemeImage] = React.useState("")
+  /**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+    
 
   function getMemeImage() {
     const memesArray = memesData.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
-    url = memesArray[randomNumber].url
-    console.log(url)
+    setMemeImage(memesArray[randomNumber].url)
   }
   return (
     <main>
-      <p>{url}</p>
       <div className='form'>
         <input type="text" placeholder="Top Text" className='form--inputs'/>
         <input type="text" placeholder="Bottom Text" className='form--inputs'/>
         <button className='form--button' onClick={getMemeImage}>Get a new meme image  🖼</button>
       </div>
+      <img src={memeImage} alt="Memes" className="meme--image"/>
     </main>
   )
 }
