7625e6390b3a767654f6671d16addb195f6ac1b7
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-05 11:20:12 +0500

    using an async function inside useEffect

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React from "react"
 
 export default function Meme() {
@@ -9,12 +8,25 @@ export default function Meme() {
     })
     const [allMemes, setAllMemes] = React.useState([])
     
+    /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+    
     React.useEffect(() => {
-      fetch("https://api.imgflip.com/get_memes") 
-        .then(res => res.json())
-        .then(data => setAllMemes(data.data.memes))
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
     }, [])
-  
+    
     function getMemeImage() {
         const randomNumber = Math.floor(Math.random() * allMemes.length)
         const url = allMemes[randomNumber].url
@@ -22,7 +34,6 @@ export default function Meme() {
             ...prevMeme,
             randomImage: url
         }))
-        
     }
     
     function handleChange(event) {
@@ -60,7 +71,7 @@ export default function Meme() {
                 </button>
             </div>
             <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
+                <img src={meme.randomImage} className="meme--image" alt="looggg"/>
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
